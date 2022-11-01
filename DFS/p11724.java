2eb5d911722914e56f0f074b79ba08806dc022d9
iiii666 <iiii666@naver.com>
2022-11-01 09:40:09 +0900

    1101

---
+++ DFS/p11724.java
@@ -0,0 +1,58 @@
+package DFS;
+
+import java.beans.Visibility;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class p11724 {
+	static List<Integer>[] li;
+	static Boolean[] visited;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		li = new ArrayList[N + 1];// 연결 리스트 생성
+		visited = new Boolean[N + 1]; // 노드들이 방문했는지 안했는지 아는 배열
+		for (int i = 1; i < N + 1; i++) {
+			li[i] = new ArrayList<Integer>();
+			visited[i] = false;
+		}
+		for (int i = 0; i < M; i++) {// 인접 리스트 초기화
+			st = new StringTokenizer(br.readLine());
+			int e = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			li[e].add(v);
+			li[v].add(e);
+
+		}
+		int count = 0;
+		for (int i = 1; i < N + 1; i++) {
+			if (visited[i] == false) {
+				count++;
+				DFS(i);
+			}
+		}
+		System.out.println(count);
+	}
+
+	static void DFS(int v) {
+		if (visited[v] == true) {
+			return;
+		}
+		visited[v] = true;
+		for (int i : li[v]) {
+			if (visited[i] == false) {
+				DFS(i);
+			}
+		}
+	}
+
+}
---
+++ DFS/p24479.java
@@ -0,0 +1,55 @@
+package DFS;
+
+import java.io.*;
+import java.util.*;
+
+public class p24479 {
+	static List<Integer>[] li;
+	static boolean[] visited;
+	static int[] route;
+	static int temp = 1;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		route = new int[N + 1];
+		li = new ArrayList[N + 1];
+		visited = new boolean[N + 1];
+		for (int i = 1; i < N + 1; i++) {
+			li[i] = new ArrayList<Integer>();
+			visited[i] = false;
+		}
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			li[u].add(v);
+			li[v].add(u);
+		}
+		route[R] = temp;
+		DFS(R);
+		for (int i = 1; i < route.length; i++) {
+			System.out.println(route[i]);
+		}
+
+	}
+
+	static void DFS(int u) {
+		if (visited[u] == true) {
+			return;
+		}
+		visited[u] = true;
+		Collections.sort(li[u]);
+		for (int i : li[u]) {
+			if (visited[i] == false) {
+				temp++;
+				route[i] = temp;
+				DFS(i);
+			}
+		}
+	}
+}
